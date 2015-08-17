279321f4276d394c8f0603e6d14d595f66f5da22
Troy Warr <troy3000@gmail.com>
2015-08-16 19:18:48 -0500

    create basic version

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+Parse.initialize('KNeNISI8FsAEmumsazFZkIN4z4xHwvkrdyDcEcWB', 'kHQp0CnhdtWGC3r8tenZ0RsdJA61DrnjwvgWE0rE');
+
+var Log = Parse.Object.extend('Log'),
+    query = new Parse.Query(Log);
+
+query.descending('createdAt');
+query.first({
+  success: function(results) {
+    console.log(results);
+    console.log(results.get('status'));
+    var start = results.get('status'),
+        $body = $('body');
+    if (start) { // yep
+      $body.addClass('yep');
+    } else { // nope
+      $body.addClass('nope');
+    }
+    $body.show();
+  },
+  error: function(error) {
+    alert('Error: ' + error.code + ' ' + error.message);
+  }
+});
